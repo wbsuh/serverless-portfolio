d1e7e5f316aaeb1a9a774720b1f15131735f8739
wbsuh <wonbae_suh@hotmail.com>
2019-05-11 19:57:00 +0900

    add portfolio lambda function

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,23 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket("portfolio.mingeegee.com")
+
+
+build_bucket = s3.Bucket("portfoliobuild.mingeegee.com")
+
+portfolio_zip = StringIO.StringIO()
+
+build_bucket.download_fileobj('portfoliobuid.zip',portfolio_zip)
+
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj,nm,
+            ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
